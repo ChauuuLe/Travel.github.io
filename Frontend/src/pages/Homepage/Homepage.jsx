7788bf55a039d3df40b8ef9cb0a50cebfb113bd1
hippo2311 <tobaochau05@gmail.com>
2024-06-05 15:25:10 +0700

    modified:   Frontend/src/assets/style.css 	modified:   Frontend/src/main.jsx 	modified:   Frontend/src/pages/Homepage/Homepage.css 	modified:   Frontend/src/pages/Homepage/Homepage.jsx 	modified:   Frontend/src/pages/navbar/Navbar.css

--- Frontend/src/pages/Homepage/Homepage.jsx
+++ Frontend/src/pages/Homepage/Homepage.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import "../Homepage/Homepage.css";
 import Navbar from '../navbar/navbar';
-import video from '../../../../Backend (server and db)/app/assets/images/video.mp4'
+import video from '../../../../Backend (server and db)/app/assets/images/video.mp4';
 
 const HomePage = () => {
   return (
-    <div className="homepage">
-
+    <section className="homepage">
+      <Navbar />
+      <div className='overlay'></div>
       <video src={video} muted autoPlay loop type="video/mp4"></video>
-      <Navbar/>
-      <div className="main-content">
+      <div className='homeContent container'>
+        <div className='textDiv'>
+          <span className='smallText'>Choose Your Place</span>
+          <h1 className='homeTitle'>Search your Holiday</h1>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
