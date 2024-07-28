be8d89b2af56b203c2e1caa3db39488a2c556045
ChauuuuLe <lehuychau130105@gmail.com>
2024-07-29 03:29:00 +0700

    Resolve conflicts

--- Frontend/src/pages/UserProfile/UserProfile.jsx
+++ Frontend/src/pages/UserProfile/UserProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
-import './userProfile.css';
+import './UserProfile.css';
 
 const UserProfile = () => {
   const { userId } = useParams();
