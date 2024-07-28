16d1480463650fbe3b68c5a2f18e396ed020236c
ChauuuuLe <lehuychau130105@gmail.com>
2024-07-29 05:01:07 +0700

    weather

--- service/weather-app-master-main-main/app/api/uv/route.ts
+++ service/weather-app-master-main-main/app/api/uv/route.ts
@@ -2,30 +2,23 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
-    const { searchParams } = req.nextUrl;
+    const searchParams = req.nextUrl.searchParams;
 
     const lat = searchParams.get("lat");
     const lon = searchParams.get("lon");
 
-    if (!lat || !lon) {
-      return new Response("Missing required query parameters: lat or lon", { status: 400 });
-    }
-
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=uv_index_max,uv_index_clear_sky_max&timezone=auto&forecast_days=1`;
 
     const res = await fetch(url, {
-      headers: { 'Content-Type': 'application/json' },
       next: { revalidate: 900 },
     });
 
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
-
     const uvData = await res.json();
+
     return NextResponse.json(uvData);
   } catch (error) {
-    console.error("Error Getting Uv Data:", error);
+    console.log("Error Getting Uv Data");
+
     return new Response("Error getting Uv Data", { status: 500 });
   }
 }
